c43cf0babc92c3443debaa855b5d73650bc53043
David Cantoran <davidcantoran36@gmail.com>
2019-12-06 17:34:39 +0000

    Automatically backed up by Learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -5,12 +5,8 @@ def fourth_challenge
       :matriarch => {name: "Lady Montague", age: "54"},
       :hero => {name: "Romeo", age: "15", status: "alive"},
       :hero_friends => [
-        {name: "Benvolio",
-        age: "17",
-        attitude: "worried",},
-        {name: "Mercutio",
-        age: "18",
-        attitude: "hot-headed"},
+         {name: "Benvolio", age: "17", attitude: "worried"},
+         {name: "Steven", age: "30", attitude: "confused"}
       ]
    }, 
    :capulet => {
@@ -18,15 +14,11 @@ def fourth_challenge
       :matriarch => {name: "Lady Capulet", age: "51"},
       :heroine => {name: "Juliet", age: "15", status: "alive"},
       :heroine_friends => [
-        {name: "Steven",
-        age: "30",
-        attitude: "confused"},
-        {name: "Nurse",
-        age: "44",
-        attitude: "worried"},
+          {name: "Mercutio", age: "18", attitude: "hot-headed"}, 
+          {name: "Nurse", age: "44", attitude: "worried"}
       ]
    }
-}
+  }
 
   
 
